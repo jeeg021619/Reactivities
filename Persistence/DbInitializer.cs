172e340d28da51d36ae84daba49df970c13ec0d2
Ryann <jeeg@codev.com>
2025-04-28 16:45:22 +0800

    End of section 12

--- Persistence/DbInitializer.cs
+++ Persistence/DbInitializer.cs
@@ -1,12 +1,35 @@
 using System;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence;
 
 public class DbInitializer
 {
-    public static async Task SeedData(AppDbContext context) 
+    public static async Task SeedData(AppDbContext context, UserManager<User> userManager) 
     {
+        if (!userManager.Users.Any()) 
+        {
+            var users = new List<User>
+                {
+                    new() {DisplayName = "Bob", UserName = "bob@test.com", Email = "bob@test.com"},
+                    new() {DisplayName = "Tom", UserName = "tom@test.com", Email = "tom@test.com"},    
+                    new() {DisplayName = "Jane", UserName = "jane@test.com", Email = "jane@test.com"},             
+                };    
+            foreach (var user in users)
+            {
+                 var result = await userManager.CreateAsync(user, "Pa$$word1");
+                    if (!result.Succeeded)
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            Console.WriteLine($"Error creating user {user.UserName}: {error.Description}");
+                        }
+                    }
+            }
+          
+        }
+
         if (context.Activities.Any()) return;
         var activities = new List<Activity>
         {
